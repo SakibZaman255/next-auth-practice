2b3ec97e1e2f1250651bc9eb7725f04e835781c6
SakibZaman255 <sakibzaman255@gmail.com>
2025-02-18 10:54:09 +0600

    loading page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import AuthForm from "@/components/AuthForm";
 import Image from "next/image";
+import Loading from "./components/Loading";
 
 export default function Home() {
   return (
     <>
-    <AuthForm/>
+    {/* <AuthForm/> */}
+    <Loading/>
     </>
   );
 }
